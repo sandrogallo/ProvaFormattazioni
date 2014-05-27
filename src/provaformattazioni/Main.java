4a72ffc985515b8d685730cb0ee8afdc86a80338
sandro.gallo <sandro.gallo@LabinfDoc.belluzzifioravanti.local>
2014-05-27 08:44:02 +0200

    Version 1.0

---
+++ src/provaformattazioni/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package provaformattazioni;
+
+/**
+ *
+ * @author sandro.gallo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        for (float f=8; f<12; f+=0.5) {
+            System.out.printf("%6.2f\n", f);
+        }
+
+    }
+}
